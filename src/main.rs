5a591f8121c1cbf8ab33bdd5ea0a87ec866b72b9
NT <lbx25@list.ru>
2026-06-26 13:37:48 +0300

    Gravitation!
    Работает бетта-центральная-гравитация

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use bevy_rapier2d::prelude::*;
 use rand::Rng;
 
 static PI: f32 = std::f32::consts::PI;
-static G: f32 = 6.67 * 0.00000000001;
+static G: f32 = 6.67 * 0.000_000_000_01;
 
 #[derive(Component)]
 struct Planet;
@@ -49,57 +49,52 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     let mut rng = rand::thread_rng();
-    // Камера (новый синтаксис 0.15)
     commands.spawn((Camera2d, Campos(10.0)));
 
-    
-        let x = -500.0;
-        let y =  0.0;
+    let x = -500.0;
+    let y = 0.0;
 
-        commands
-            .spawn((
-                Mesh2d(meshes.add(Circle::new(40.0))),
-                MeshMaterial2d(materials.add(Color::srgba(0.086, 0.259, 0.157, 1.0))),
-                Transform::from_xyz(x, y, 0.1),
-                RigidBody::Dynamic,
-                Collider::ball(50.0),
-                Velocity::linear(Vec2::new(0.0, 0.0)),
-                GravityScale(0.0),
-                AdditionalMassProperties::Mass(0.5),
-                ExternalForce {
-                    force: Vec2::new(0.0, 0.0), // Сила в Ньютонах (вправо)
-                    torque: 0.0,
-                },
-                Dir(0.0),
-                Rec,
-            ))
-            .with_children(|parent| {
-                parent.spawn((
-                    Mesh2d(meshes.add(Circle::new(50.0))),
-                    MeshMaterial2d(materials.add(Color::from(bevy::color::palettes::basic::BLACK))),
-                    Transform::from_xyz(0.0, 0.0, 0.0),
-                ));
-                parent.spawn((
-                    Mesh2d(meshes.add(Circle::new(5.0))),
-                    MeshMaterial2d(materials.add(Color::from(bevy::color::palettes::basic::BLACK))),
-                    Transform::from_xyz(35.0, 0.0, 0.2),
-                ));
-
-                parent.spawn((
-                    Mesh2d(meshes.add(Rectangle::new(20.0, 140.0))),
-                    MeshMaterial2d(
-                        materials.add(Color::from(Color::srgba(0.086, 0.259, 0.157, 1.0))),
-                    ),
-                    Transform::from_xyz(-45.0, 0.0, 0.1),
-                ));
-                parent.spawn((
-                    Mesh2d(meshes.add(Rectangle::new(30.0, 150.0))),
-                    MeshMaterial2d(materials.add(Color::from(bevy::color::palettes::basic::BLACK))),
-                    Collider::cuboid(15.0, 75.0),
-                    Transform::from_xyz(-45.0, 0.0, 0.0),
-                ));
-            });
-    
+    commands
+        .spawn((
+            Mesh2d(meshes.add(Circle::new(5.0))),
+            MeshMaterial2d(materials.add(Color::srgba(0.086, 0.259, 0.157, 1.0))),
+            Transform::from_xyz(x, y, 0.1),
+            RigidBody::Dynamic,
+            Collider::ball(5.0),
+            Velocity::linear(Vec2::new(0.0, 0.0)),
+            GravityScale(0.0),
+            AdditionalMassProperties::Mass(5000.0),
+            ExternalForce {
+                force: Vec2::new(0.0, 0.0), // Сила в Ньютонах (вправо)
+                torque: 0.0,
+            },
+            Dir(0.0),
+            Rec,
+        ))
+        .with_children(|parent| {
+            parent.spawn((
+                Mesh2d(meshes.add(Circle::new(5.0))),
+                MeshMaterial2d(materials.add(Color::from(bevy::color::palettes::basic::BLACK))),
+                Transform::from_xyz(0.0, 0.0, 0.0),
+            ));
+            parent.spawn((
+                Mesh2d(meshes.add(Circle::new(1.0))),
+                MeshMaterial2d(materials.add(Color::from(bevy::color::palettes::basic::BLACK))),
+                Transform::from_xyz(1.0, 0.0, 0.2),
+            ));
+/*
+            parent.spawn((
+                Mesh2d(meshes.add(Rectangle::new(20.0, 140.0))),
+                MeshMaterial2d(materials.add(Color::from(Color::srgba(0.086, 0.259, 0.157, 1.0)))),
+                Transform::from_xyz(-45.0, 0.0, 0.1),
+            ));
+            parent.spawn((
+                Mesh2d(meshes.add(Rectangle::new(30.0, 150.0))),
+                MeshMaterial2d(materials.add(Color::from(bevy::color::palettes::basic::BLACK))),
+                Collider::cuboid(15.0, 75.0),
+                Transform::from_xyz(-45.0, 0.0, 0.0),
+            ));
+ */       });
 }
 
 fn world_spawn(
@@ -107,11 +102,11 @@ fn world_spawn(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    let radius = 1000.0;
+    let radius = 128.0;
     let pos_x = 100.0;
     let pos_y = 100.0;
     let pos_z = 0.0;
-    let density = 5515000000000.0;
+    let density = 274_000_000.0;
 
     for i in 0..1 {
         commands.spawn((
@@ -126,7 +121,7 @@ fn world_spawn(
             PlanetDensity(density),
             PlanetPreGravity(0.0),
             ExternalForce {
-                force: Vec2::new(0.0, 0.0), // Сила в Ньютонах (вправо)
+                force: Vec2::new(0.0, 0.0), 
                 torque: 0.0,
             },
             GravityScale(0.0),
@@ -147,19 +142,19 @@ fn player_system(
 ) {
     for (mut velocity, transform, mut dir) in &mut player_query {
         if keys.pressed(KeyCode::KeyD) {
-            velocity.angvel -= time.delta_secs() *200.0 * PI / 180.0;
+            velocity.angvel -= time.delta_secs() * 200.0 * PI / 180.0;
         }
         if keys.pressed(KeyCode::KeyA) {
-            velocity.angvel += time.delta_secs() *200.0 * PI / 180.0;
+            velocity.angvel += time.delta_secs() * 200.0 * PI / 180.0;
         }
 
         if keys.pressed(KeyCode::KeyW) {
-            velocity.linvel.x += time.delta_secs() * dir.0.cos()*200.0;
-            velocity.linvel.y += time.delta_secs() * dir.0.sin()*200.0;
+            velocity.linvel.x += time.delta_secs() * dir.0.cos() * 200.0;
+            velocity.linvel.y += time.delta_secs() * dir.0.sin() * 200.0;
         }
         if keys.pressed(KeyCode::KeyS) {
-            velocity.linvel.x -= time.delta_secs() * dir.0.cos()*200.0;
-            velocity.linvel.y -= time.delta_secs() * dir.0.sin()*200.0;
+            velocity.linvel.x -= time.delta_secs() * dir.0.cos() * 200.0;
+            velocity.linvel.y -= time.delta_secs() * dir.0.sin() * 200.0;
         }
 
         let forward = transform.local_x();
@@ -174,13 +169,16 @@ fn player_system(
 }
 
 fn planet_prepare(
-    mut planet_query: Query<( &mut PlanetPreGravity,&mut AdditionalMassProperties,&PlanetDensity,&PlanetVolume), With<Planet>,>,
+    mut planet_query: Query<
+        (
+            &mut PlanetPreGravity,
+            &mut AdditionalMassProperties,
+            &PlanetDensity,
+            &PlanetVolume,
+        ),
+        With<Planet>,
+    >,
 ) {
-
-     println!(
-            " Planet prepare!)"
-        );
-
     for (mut pre_gravity, mut mass, density, volume) in &mut planet_query {
         *mass = AdditionalMassProperties::Mass(density.0 * volume.0);
         let mut get_mass = 0.0;
@@ -190,7 +188,9 @@ fn planet_prepare(
         }
 
         pre_gravity.0 = G * get_mass;
+           
     }
+
 }
 
 fn world_gravity_sistem(
@@ -199,16 +199,25 @@ fn world_gravity_sistem(
 ) {
     for (transform, mut external_force) in &mut player_query {
         let mut full_ext_forse = (0.0, 0.0);
-        for(planet_pre_gravity, external_force_planet, transform_planet) in &mut planet_query{
-                let range = ((transform_planet.translation.x - transform.translation.x).powf(2.0) +  (transform_planet.translation.y - transform.translation.y).powf(2.0)).powf(0.5);
+        for (planet_pre_gravity, mut external_force_planet, transform_planet) in &mut planet_query {
+            let range = ((transform_planet.translation.x - transform.translation.x).powf(2.0)
+                + (transform_planet.translation.y - transform.translation.y).powf(2.0))
+            .powf(0.5);
 
-                full_ext_forse.0 += planet_pre_gravity.0/range.powf(3.0) * (transform_planet.translation.x - transform.translation.x);
-                full_ext_forse.1 += planet_pre_gravity.0/range.powf(3.0) * (transform_planet.translation.y - transform.translation.y);
+            full_ext_forse.0 += planet_pre_gravity.0 / range.powf(3.0)
+                * (transform_planet.translation.x - transform.translation.x) * 50000.0;
+            full_ext_forse.1 += planet_pre_gravity.0 / range.powf(3.0)
+                * (transform_planet.translation.y - transform.translation.y)* 50000.0;
 
-        }  
-        external_force.force.x = full_ext_forse.0;
-        external_force.force.y = full_ext_forse.1;
-        println!("{} {}", external_force.force.x, external_force.force.y);
+            external_force_planet.force.x = -full_ext_forse.0;
+            external_force_planet.force.y = -full_ext_forse.1;
+
+            external_force.force.x = full_ext_forse.0;
+            external_force.force.y = full_ext_forse.1;
+            
+        println!("{}   {}",range,( external_force.force.x.powf(2.0) + external_force.force.y.powf(2.0)).powf(0.5));
+
+        }
     }
 }
 
